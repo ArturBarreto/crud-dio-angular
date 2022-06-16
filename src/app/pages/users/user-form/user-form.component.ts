73867f537bd3293d8c1c1f28052857dc59b786eb
ArturBarreto <artur.gomes.barreto@gmail.com>
2022-06-16 18:10:33 -0300

    Edição de usuários

--- src/app/pages/users/user-form/user-form.component.ts
+++ src/app/pages/users/user-form/user-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 import { User } from 'src/app/models/user';
 import { UserService } from 'src/app/services/user.service';
 
@@ -11,18 +12,40 @@ import { UserService } from 'src/app/services/user.service';
 export class UserFormComponent implements OnInit {
   users: User[] = [];
   userForm: FormGroup;
+  userId: any = '';
 
-  constructor(private fb: FormBuilder, private userService: UserService) { 
-    this.userForm = this.fb.group({
-      id: 0,
-      nome: '',
-      sobrenome: '',
-      idade: '',
-      profissao: '' 
+  constructor(
+    private fb: FormBuilder, 
+    private userService: UserService, 
+    private actRoute: ActivatedRoute,
+    private router: Router) { 
+
+      this.userForm = this.fb.group({
+        id: 0,
+        nome: '',
+        sobrenome: '',
+        idade: '',
+        profissao: '' 
     })
   }
 
   ngOnInit(): void {
+    this.actRoute.paramMap.subscribe(params => {
+      this.userId = params.get('id');
+      console.log(this.userId);
+      if(this.userId !== null) {
+        this.userService.getUser(this.userId).subscribe(result => {
+          this.userForm.patchValue({
+            id: result[0].id,
+            nome: result[0].nome,
+            sobrenome: result[0].sobrenome,
+            idade: result[0].idade,
+            profissao: result[0].profissao
+          })
+        })
+      }
+    });
+
     this.getUsers();
   }
 
@@ -48,6 +71,24 @@ export class UserFormComponent implements OnInit {
       });
       this.getUsers();
     });
+  }
+
+  updateUser() {
+    this.userService.updateUser(this.userId, this.userForm.value).subscribe(result => {
+      console.log("Usuário atualizado " + result);
+    }, (err) => {
+      console.log(err);
+    }, () => {
+      this.router.navigate(['/']);
+    });
+  }
+
+  actionButton() {
+    if(this.userId !== null) {
+      this.updateUser(); 
+    } else {
+      this.createUser;
+    }
   }
 
 }
